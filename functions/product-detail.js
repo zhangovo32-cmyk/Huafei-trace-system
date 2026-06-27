94cc1174143aa7a91539744bc2881d6c7c9123a1
zhangovo32-cmyk <zhangovo32@gmail.com>
2026-06-27 13:58:27 +0800

    Fix product detail redirect loop

--- functions/product-detail.js
+++ functions/product-detail.js
@@ -3,19 +3,5 @@ export async function onRequest(context) {
   url.pathname = "/product-detail.html";
   url.search = "";
 
-  const request = new Request(url.toString(), context.request);
-  const response = context.env?.ASSETS?.fetch
-    ? await context.env.ASSETS.fetch(request)
-    : await fetch(request);
-
-  const headers = new Headers(response.headers);
-  headers.set("cache-control", "no-store, no-cache, must-revalidate, max-age=0");
-  headers.set("pragma", "no-cache");
-  headers.set("expires", "0");
-
-  return new Response(response.body, {
-    status: response.status,
-    statusText: response.statusText,
-    headers
-  });
+  return Response.redirect(url.toString(), 302);
 }
