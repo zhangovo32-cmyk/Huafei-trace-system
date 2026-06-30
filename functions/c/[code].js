2bb3e5817ba660abacf07042db8a1cf86d35222a
zhangovo32-cmyk <zhangovo32@gmail.com>
2026-06-30 21:14:53 +0800

    Serve check page without redirect

--- functions/c/[code].js
+++ functions/c/[code].js
@@ -1,12 +1,21 @@
-import { QR_LINK_VERSION } from "../_shared/qr-print.js";
-
 export async function onRequest(context) {
   const url = new URL(context.request.url);
-  const code = context.params?.code || url.pathname.split("/").filter(Boolean).pop();
-  url.pathname = "/check";
+  url.pathname = "/check.html";
   url.search = "";
-  if (code) url.searchParams.set("code", String(code));
-  url.searchParams.set("v", QR_LINK_VERSION);
 
-  return Response.redirect(url.toString(), 302);
+  const request = new Request(url.toString(), context.request);
+  const response = context.env?.ASSETS?.fetch
+    ? await context.env.ASSETS.fetch(request)
+    : await fetch(request);
+
+  const headers = new Headers(response.headers);
+  headers.set("cache-control", "no-store, no-cache, must-revalidate, max-age=0");
+  headers.set("pragma", "no-cache");
+  headers.set("expires", "0");
+
+  return new Response(response.body, {
+    status: response.status,
+    statusText: response.statusText,
+    headers
+  });
 }
